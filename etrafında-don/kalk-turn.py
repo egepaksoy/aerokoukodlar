851d1a06689a83002ef449ee1761a4399118b682
Ege Paksoy <egepaskoy@gmail.com>
2024-12-06 17:10:11 +0300

    gitme ve donme, kalkma ve donme

---
+++ etrafında-don/kalk-turn.py
@@ -0,0 +1,59 @@
+import time
+import sys
+sys.path.append('./pymavlink_custom')
+from pymavlink_custom import *
+from pymavlink import mavutil
+import threading
+
+def failsafe(vehicle):
+    def failsafe_drone_id(vehicle, drone_id):
+        print(f"{drone_id}>> Failsafe alıyor")
+        vehicle.set_mode(mode="RTL", drone_id=drone_id)
+
+    thraeds = []
+    for d_id in vehicle.drone_ids:
+        thrd = threading.Thread(target=failsafe_drone_id, args=(vehicle, d_id))
+        thrd.start()
+        thraeds.append(thrd)
+
+    for t in thraeds:
+        t.join()
+
+    print(f"Dronlar {vehicle.drone_ids} Failsafe aldi")
+
+if len(sys.argv) != 3:
+    print("Usage: python main.py <connection_string> <drone_id>")
+    sys.exit(1)
+
+vehicle = Vehicle(sys.argv[1])
+
+start_time = time.time()
+
+ALT = 3
+DRONE_ID = int(sys.argv[2]) # drone id
+
+try:
+    vehicle.set_mode(mode="GUIDED", drone_id=DRONE_ID)
+    vehicle.arm_disarm(arm=True, drone_id=DRONE_ID)
+    vehicle.takeoff(ALT, drone_id=DRONE_ID)
+    
+    print(f"{DRONE_ID}>> takeoff yaptı")
+
+    vehicle.turn_around(drone_id=DRONE_ID)
+
+    vehicle.set_mode(mode="LAND", drone_id=DRONE_ID)
+    
+    print("Görev tamamlandı")
+
+except KeyboardInterrupt:
+    print("Exiting...")
+    failsafe(vehicle)
+
+except Exception as e:
+    failsafe(vehicle)
+    print(e)
+
+finally:
+    vehicle.vehicle.close()
+
+
