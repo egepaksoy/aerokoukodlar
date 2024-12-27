7eb99a248e068ec0b787f880148962f4fe06d651
Ege Paksoy <egepaskoy@gmail.com>
2024-12-27 20:31:49 +0300

    goruntu islemede hedef ekleme, kaldirma yapildi

--- goruntuisleme/anlık-isleme/detect.py
+++ goruntuisleme/anlık-isleme/detect.py
@@ -1,53 +1,151 @@
-import tensorflow as tf
-import numpy as np
+#! GIBI
+import sys
 import cv2
+import numpy as np
+from ultralytics import YOLO
+import os
+import time
+import keyboard
 
-# Eğitilmiş modeli yükle
-model = tf.keras.models.load_model("./models/mobilenet_trained_model.h5")
+def is_centered(konum, ekran_orani, orta_pozisyon_orani: float = 0.4) -> bool:
+    x, y = konum
+    ekran_genislik, ekran_yukseklik = ekran_orani
+    # Merkezdeki alanın sınırlarını belirle     baslangıc, bitis
+    x_orta_genisligi = ekran_genislik * orta_pozisyon_orani
+    y_orta_genisligi = ekran_yukseklik * orta_pozisyon_orani
 
-# Sınıf adlarını manuel olarak tanımla (eğitim sırasında kullandığın klasör adlarına göre ayarla)
-class_indices = {0: "trophy"}  # Örnek: {0: "nesne1", 1: "nesne2"}
+    x_uzaklik = 0
+    y_uzaklik = 0
 
-# Girdi boyutları (MobileNetV2 için 224x224)
-IMG_SIZE = (224, 224)
+    if x > ekran_genislik * (0.5 + orta_pozisyon_orani / 2):
+        x_uzaklik = x - ekran_genislik * (0.5 + orta_pozisyon_orani / 2)
+    elif x < ekran_genislik * (0.5 - orta_pozisyon_orani / 2):
+        x_uzaklik = x - ekran_genislik * (0.5 - orta_pozisyon_orani / 2)
+    
+    if y > ekran_yukseklik * (0.5 + orta_pozisyon_orani / 2):
+        y_uzaklik = y - ekran_yukseklik * (0.5 + orta_pozisyon_orani / 2)
+    elif y < ekran_yukseklik * (0.5 - orta_pozisyon_orani / 2):
+        y_uzaklik = y - ekran_yukseklik * (0.5 - orta_pozisyon_orani / 2)
+    
+    return x_uzaklik, y_uzaklik
 
-# Kamera akışı başlat
-cap = cv2.VideoCapture(0)
+
+def center_image(konum, ekran_orani, orta_pozisyon_orani: float = 0.4):
+    x_uzakligi, y_uzakligi = is_centered(konum, ekran_orani, orta_pozisyon_orani)
+
+    islem = {"x": "", "y": ""}
+
+    if x_uzakligi == 0:
+        islem["x"] = "Merkezde"
+    elif x_uzakligi > 0:
+        islem["x"] = f"Sola git: {x_uzakligi:.2f}"
+    else:
+        islem["x"] = f"Saga git: {x_uzakligi:.2f}"
+
+    if y_uzakligi == 0:
+        islem["y"] = "Merkezde"
+    
+    elif y_uzakligi > 0:
+        islem["y"] = f"Asagiya git: {y_uzakligi:.2f}"
+    else:
+        islem["y"] = f"Yukariya git: {y_uzakligi:.2f}"
+    
+    return islem
+
+model_name = "kullanilcak.pt"
+model = YOLO("../models/" + model_name)
+
+orta_pozisyon_orani = (0.4)
+
+cap = cv2.VideoCapture(0) 
+
+applicated_classes = []
+
 if not cap.isOpened():
     print("Kamera açılamadı!")
     exit()
 
-print("Kamera başlatıldı. Çıkış için 'q' tuşuna basın.")
+try:
+    total_frame = 0
+    
+    frame_time = time.time()
+    fps = 0
+    while True:
+        ret, frame = cap.read()
+        ekran_orani = (frame.shape[1], frame.shape[0])
 
-while True:
-    ret, frame = cap.read()
-    if not ret:
-        print("Kamera görüntüsü alınamadı!")
-        break
+        if not ret:
+            print("Kare okunamadı!")
+            break
 
-    # Görüntüyü modelin girdi boyutlarına yeniden boyutlandır
-    resized_frame = cv2.resize(frame, IMG_SIZE)
-    input_image = np.expand_dims(resized_frame, axis=0) / 255.0  # Normalleştirme
+        if frame is not None:
+            results = model(frame)
+            for r in results:
+                boxes = r.boxes
+                for box in boxes:
+                    if box.conf[0] < 0.90:
+                        continue
 
-    # Tahmin yap
-    predictions = model.predict(input_image)
-    predicted_class = np.argmax(predictions)
-    confidence = np.max(predictions)
+                    # Sınıf ve güven skorunu al
+                    cls = int(box.cls[0].item())
+                    conf = box.conf[0].item()
 
-    # Tahmini sınıf adını al
-    class_name = class_indices.get(predicted_class, "Bilinmeyen")
+                    # Sınıf adını al
+                    class_name = model.names[cls]
 
-    # Sonuçları görüntü üzerine yaz
-    text = f"{class_name} ({confidence * 100:.2f}%)"
-    cv2.putText(frame, text, (10, 50), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
+                    if keyboard.is_pressed("c") and class_name not in applicated_classes:
+                        applicated_classes.append(class_name)
+                        cv2.putText(frame, f"{class_name} sınıfı eklendi", (0, 50), 
+                                cv2.FONT_HERSHEY_SIMPLEX, 0.9, (0, 0, 255), 2)
 
-    # Görüntüyü göster
-    cv2.imshow("Nesne Tanıma", frame)
+                    if class_name not in applicated_classes:
+                        continue
 
-    # Çıkış için 'q' tuşuna bas
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        break
+                    # Sınırlayıcı kutu koordinatlarını al
+                    x1, y1, x2, y2 = box.xyxy[0].cpu().numpy()
 
-# Kaynakları serbest bırak
-cap.release()
-cv2.destroyAllWindows()
+                    # Bilgileri ekrana yazdır
+                    print(f"Sınıf: {class_name}, Güven: {conf:.2f}, Konum: ({x1:.0f}, {y1:.0f}, {x2:.0f}, {y2:.0f}")
+                    konumu = (x1 + (x2-x1)/2, y1 + (y2-y1)/2)
+                    
+                    print(center_image(konumu, ekran_orani, orta_pozisyon_orani))
+
+                    # Nesneyi çerçeve içine al ve etiketle
+                    cv2.rectangle(frame, (int(x1), int(y1)), (int(x2), int(y2)), (0, 0, 255), 2)
+                    cv2.circle(frame, (int(konumu[0]), int(konumu[1])), 5, (0, 0, 255), -1)
+
+                    cv2.putText(frame, f"{class_name} {conf:.2f}", (int(x1), int(y1 - 10)), 
+                                cv2.FONT_HERSHEY_SIMPLEX, 0.9, (0, 255, 0), 2)
+                    
+                    cv2.putText(frame, f"{center_image(konumu, ekran_orani, orta_pozisyon_orani)}", (int(ekran_orani[0] * (0.5 - orta_pozisyon_orani / 2)), int(ekran_orani[1] * (0.5 + orta_pozisyon_orani / 2))), 
+                                cv2.FONT_HERSHEY_SIMPLEX, 0.9, (0, 255, 0), 3)
+                    
+
+            if time.time() - frame_time:
+                fps = total_frame / (time.time() - frame_time)
+                total_frame = 0
+                frame_time = time.time()
+
+            cv2.putText(frame, f"{fps:.1f}", (0, 25), 
+                        cv2.FONT_HERSHEY_SIMPLEX, 0.9, (0, 255, 0), 2)
+            cv2.rectangle(frame, (int(ekran_orani[0] * (0.5 - orta_pozisyon_orani / 2)), int(ekran_orani[1] * (0.5 - orta_pozisyon_orani / 2))), (int(ekran_orani[0] * (0.5 + orta_pozisyon_orani / 2)), int(ekran_orani[1] * (0.5 + orta_pozisyon_orani / 2))), (255, 0, 0), 2)
+            cv2.imshow('YOLOv8 Canlı Tespit', frame)  # Görüntüyü göster
+                
+            total_frame += 1
+
+        # Çıkış için 'q' tuşuna basılması beklenir
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+        
+        if keyboard.is_pressed("d") and len(applicated_classes) > 0:
+            cv2.putText(frame, f"{applicated_classes[-1]} sınıfı cikarildi", (0, 50), 
+                    cv2.FONT_HERSHEY_SIMPLEX, 0.9, (0, 0, 255), 2)
+            applicated_classes.remove(applicated_classes[-1])
+
+        
+except KeyboardInterrupt:
+    print("Ctrl+C ile çıkıldı.")
+
+finally:
+    print("Program sonlandırıldı.")
+    cv2.destroyAllWindows()  # Tüm OpenCV pencerelerini kapat
